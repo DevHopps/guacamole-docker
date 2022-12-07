1e98d1dc7e9ba6f9579071846701ddf3357d28da
Benedict Balzer <mail@hopps.me>
2022-12-07 20:37:02 +0100

    Try fixing paths & script

--- init_guacamole.sh
+++ init_guacamole.sh
@@ -7,23 +7,13 @@ mv initdb.sql postgres-init/initdb.sql
 echo "DB done!"
 echo "Starting container using docker compose"
 
-docker compose pull
-docker compose up -d
-
-echo "Waiting 30s before installing TOTP plugin..."
-echo "If you don't want to install this plugin you can stop this script by pressing ctrl + c"
-
-sleep 30s
-
-echo "Stopping containers..."
-docker compose down
-
 echo "Installing plugins to correct folder"
+mkdir guacamole_home
 mkdir guacamole_home/extensions
 mv guacamole-auth-totp-1.4.0.jar guacamole_home/extensions/guacamole-auth-totp-1.4.0.jar
 
-echo "Done installing, starting containers!"
+docker compose pull
 docker compose up -d
 
 sleep 5
-echo "Guacamole should now be reachable via http://<host>:<port>!"
+echo "Guacamole should now be reachable via http://<host>:<port>/guacamole!"
