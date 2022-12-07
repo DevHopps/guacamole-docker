0e5bbfb699bdefcf957600297f9943e5b7dc0b42
Benedict Balzer <mail@hopps.me>
2022-12-07 18:43:29 +0100

    Initial project files

---
+++ init_guacamole.sh
@@ -0,0 +1,22 @@
+echo "Creating required database..."
+
+mkdir ./postgres-init >/dev/null 2>&1
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ./postgres-init/initdb.sql
+
+echo "DB done!"
+echo "Starting container using docker compose"
+
+docker compose pull
+docker compose up -d
+
+echo "Waiting 30s before installing TOTP plugin..."
+echo "If you don't want to install this plugin you can stop this script by pressing ctrl + c"
+
+sleep 30s
+
+echo "Stopping containers..."
+docker compose down
+
+echo "Installing plugins to correct folder"
+mkdir guacamole_home/extensions
+mv guacamole-auth-totp-1.4.0.jar guacamole_home/extensions/guacamole-auth-totp-1.4.0.jar
